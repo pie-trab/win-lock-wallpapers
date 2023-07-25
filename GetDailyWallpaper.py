1e04c473595137acaf762ce2f41a523a81369cdf
pie-trab <pietrotrabuio@gmail.com>
2023-07-25 12:38:45 +0200

    Changed the algorithm and inproved performance

--- GetDailyWallpaper.py
+++ GetDailyWallpaper.py
@@ -1,61 +1,118 @@
 import os
 import shutil
+import hashlib
 from PIL import Image
 
-userPath = os.getenv('USERPROFILE')
+userPath = os.getenv("USERPROFILE")
 
 # up to 9999 wallpers
 
 # destination directory, the folder will be created if doesn't exits
-wallpaperDir = userPath + '\\Pictures\\WinWallPapers'
+wallpaperDir = userPath + "\\Pictures\\Sfondi\\Sfondi_Windows10"
 
-# asset dyrectory where the lockscreen files are stored
-assetsDir = userPath + \
-    '\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets'
+# asset dyrectory wherse the lockscreen files are stored
+assetsDir = (
+    userPath
+    + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
+)
 
 
-def checkAspectRatio(file):
-    if (Image.open(file).size[0] != 1920):
-        return False
-    else:
-        return True
+def checkAspectRatio(file, get_mobile):
+    """ Checks the aspect ratio of the input file
+
+    Args:
+        file (str): file path
+        get_mobile (bool): True - keeps mobile wallpapers, False - discards mobile wallpapers
+
+    Returns:
+        bool: the file is a wallpaper
+    """
+    return Image.open(file).size[0] == 1920 or (get_mobile and Image.open(file).size[1] == 1920)
 
 
 def justifiesName(number):
+    """ Justifies the file name to accomodate up to 9999 walpapers
+
+    Args:
+        number (int): number of the walpaper
+
+    Returns:
+        str: justified number
+    """
     return str(number).rjust(4, "0")
 
 
-def getWallpaper():
-    # get a list of all files of the directory assets on form of direntry
-    fileListAsset = []
-    for i in os.scandir(assetsDir):
-        if i.is_file():
-            fileListAsset.append(i)
+# computes the hash of the file
+def get_sha256(file):
+    """ Computes the md5 hash of the file
 
+    Args:
+        file (str): file path
+
+    Returns:
+        str: the hexadecimal digest of the hash
+    """
+    BLOCK_SIZE = 65536  # The size of each read from the file
+
+    # Create the hash object, can use something other than `.sha256()` if you wish
+    file_hash = hashlib.sha256()
+    with open(file, 'rb') as f:  # Open the file to read it's bytes
+        # Read from the file. Take in the amount declared above
+        fb = f.read(BLOCK_SIZE)
+        while len(fb) > 0:  # While there is still data being read from the file
+            file_hash.update(fb)  # Update the hash
+            fb = f.read(BLOCK_SIZE)  # Read the next block from the file
+
+    return file_hash.hexdigest()  # Get the hexadecimal digest of the hash
+
+
+def not_intersect(dict1, dict2):
+    """ Intersection of two dictionaries
+
+    Args:
+        dict1 (dict): 
+        dict2 (dict): second dictionary
+
+    Returns:
+        dict: not intesection
+    """
+    temp = {}
+
+    for i in dict1:
+        if not (i in dict2):
+            temp[i] = dict1[i]
+
+    return temp
+
+
+# main stuff
+def getWallpaper():
     # creation of destination folder if doesn't exists
-    if (not os.path.isdir(wallpaperDir)):
+    if not os.path.isdir(wallpaperDir):
         os.mkdir(wallpaperDir)
 
-    # get a list of all files of the directory desktop on form of direntry
-    fileListWallpaper = []
-    for i in os.scandir(wallpaperDir):
-        if i.is_file() and i.name:
-            fileListWallpaper.append(i)
+    # dictionary of all files respecting aspect ratio in asset directory direntry
+    fileListAsset = {}
+    for i in os.scandir(assetsDir):
+        if i.is_file() and checkAspectRatio(i.path, False):
+            fileListAsset[get_sha256(i.path)] = i
 
-    isEqual = False
-    for i in fileListAsset:
-        for j in fileListWallpaper:
-            if (open(i, 'rb').read() == open(j, 'rb').read()):
-                isEqual = True
-                if (i.name != j.name):
-                    print(i.name + ' is the same as ' + j.name)
-        if (not isEqual) and checkAspectRatio(i.path):
-            shutil.copy(i.path, wallpaperDir)
-            filesNumber = len(os.listdir(wallpaperDir)) - 1
-            print(filesNumber)
-            os.rename(wallpaperDir + '\\' + i.name, wallpaperDir + '\\' +
-                      'Lockscreen' + justifiesName((filesNumber + 1)) + '.jpg')
-        isEqual = False
+    # dictionary of all files in desktp directory direntry
+    fileListWallpaper = {}
+    for i in os.scandir(wallpaperDir):
+        if i.is_file():
+            fileListWallpaper[get_sha256(i.path)] = i
+
+    toMove = not_intersect(fileListAsset, fileListWallpaper)
+
+    initCount = len(fileListWallpaper) + 1
+
+    for i in toMove:
+        shutil.copy(toMove[i].path, wallpaperDir)
+        print("Lockscreen" + justifiesName(initCount) + ".jpg")
+        os.rename(wallpaperDir + "\\" +
+                  toMove[i].name, wallpaperDir + "\\" + "Lockscreen" + justifiesName(initCount) + ".jpg")
+        initCount += 1
 
 
 getWallpaper()
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # win-lock-wallpapers
 
-Simple script to get the windows 10/11 lockscreen wallpapers
+Simple script to get the lockscreen wallpapers
+
+This simple script gets the lockscreen wallpapers from the Assets dir in the AppData folder
+
+`# asset dyrectory wherse the lockscreen files are stored assetsDir = (     userPath     + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets" )`
+
+After discarding some useless files (in this directory are stored the banner for the preinstalled ads in windows) moves the wallpapers to a user set directory
+
+## Usage
+
+To use the program set the wallpaper dir to whatever you like the default is under "user\Pictures\LockScreenWallpapers" and it will be automatically created
+
+`# destination directory, the folder will be created if doesn't exits wallpaperDir = userPath + "\\Pictures\\Sfondi\\Sfondi_Windows10"`
+
+After that simply execute the program as you like
